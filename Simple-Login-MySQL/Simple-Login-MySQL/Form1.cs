4979a5ec4c1ff96e1d674b074763f92e3083e98f
Gary Lloyd M. Senoc <62071560+garysenoc@users.noreply.github.com>
2020-04-03 20:04:10 +0800

    Add files via upload

---
+++ Simple-Login-MySQL/Simple-Login-MySQL/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Simple_Login_MySQL
+{
+    public partial class Form1 : Form
+    {
+        public MySqlCommand sql_cmd = new MySqlCommand();
+        public string sID;
+        public string sql = "";
+        public string user;
+        public string pass;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clsMySQL.sql_con.Close();
+            clsMySQL.sql_con.Open(); // open connection for the database
+
+            sql = "SELECT *FROM tbuser";
+            sql_cmd = new MySqlCommand(sql, clsMySQL.sql_con);
+            MySqlDataReader rd = sql_cmd.ExecuteReader();
+            while (rd.Read())
+            {
+
+                user = rd["username"].ToString();
+                pass = rd["password"].ToString();
+
+            }
+            rd.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            welcome op = new welcome();
+
+            if (textBox1.Text == user && textBox2.Text == pass)
+            {
+                op.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password");
+            }
+                    
+        }
+    }
+}
---
+++ Simple-Login-MySQL/Simple-Login-MySQL/clsMySQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Simple_Login_MySQL
+{
+    class clsMySQL
+    {
+        public static string server = "127.0.0.1";
+        public static string database = "logindb";
+        public static string user = "root";
+        public static string pass = "";
+        public static string constring = "server = " + server + ";" + "database = " + database + ";" + "uid = " + user + ";" + "password = " + pass + ";";
+        public static MySqlConnection sql_con = new MySqlConnection(constring);
+    }
+}
